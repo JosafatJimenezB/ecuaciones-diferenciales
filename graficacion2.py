e6de3d66da5ef8d1075db6773a33827bcd21c705
josafat <josa221001@gmail.com>
2022-05-23 13:28:31 -0500

    add. creando funciones

--- graficacion2.py
+++ graficacion2.py
@@ -1,8 +1,25 @@
 from scipy import linalg as al
 import numpy as np
 
+def suma1 (x,y):
+    return x+y
+
+def resta1(x,y):
+    return x-y
+
+def multi(x,y):
+    return x*y
+
+
 a = np.matrix([[2,3],[-5,4]])
-print(a)
+a1 = np.matrix([[7,-1],[-3,5]])
+
+
+print('resultado de la suma es: ')
+print(suma1(a,a1))
+print('resultado de la resta es: ')
+print(resta1(a,a1))
+
 
 b = al.inv(a)
 print(b)
