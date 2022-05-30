f053abd6481f05081ba4200657750fbd227af4b5
josafat <josa221001@gmail.com>
2022-05-30 13:20:16 -0500

    add: transformada de laplace

---
+++ laplace.py
@@ -0,0 +1,10 @@
+import sympy as sp
+t=sp.Symbol('t',positive=True)
+s=sp.Symbol('s',positive=True)
+#dy=sp.cos(2*t)
+dy=sp.exp(2*t**2)
+ds=sp.laplace_transform(dy,t,s,noconds=True)
+sp.pprint(ds)
+y=sp.inverse_laplace_transform(ds,s,t,noconds=True)
+y=sp.simplify(y)
+sp.pprint(y)
